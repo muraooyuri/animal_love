7fb6fc12517a5c68183ab794fed03fe14a15f5bd
muraooyuri <yuri.0112hm@icloud.com>
2023-03-05 09:07:10 +0000

    devise機能

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+%i[
+  犬
+  猫
+  小動物
+  さかな
+  とり
+  爬虫類・その他
+].each { |name| Genre.find_or_create_by(name:) }
+genre_ids = Genre.all.pluck(:id)
