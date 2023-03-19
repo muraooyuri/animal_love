f22fd37a98b9d373df70d0a050283653c7f9a338
muraooyuri <yuri.0112hm@icloud.com>
2023-03-19 08:31:21 +0000

    ビュー編集とコントローラ追加

--- app/controllers/animal_comments_controller.rb
+++ app/controllers/animal_comments_controller.rb
@@ -1,2 +1,24 @@
 class AnimalCommentsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def create
+    @animal = Animal.find(params[:animal_id])
+    animal_comments = current_user.animal_comments.new(animal_comment_params)
+    animal_comments.animal_id = @animal.id
+    animal_comments.save
+    redirect_to animal_path(@animal)
+  end
+
+  def destroy
+    @animal = Animal.find(params[:animal_id])
+    @animal_comment=AnimalComment.find_by(id: params[:id], animal_id: params[:animal_id])
+    @animal_comment.destroy
+    redirect_to animal_path(@animal)
+  end
+
+  private
+
+  def animal_comment_params
+     params.require(:animal_comment).permit(:comment)
+  end
 end
